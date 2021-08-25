2478c2bd3948989ef41e5891a43495cbb5dee2e9
Lanxi <lanxi0701@gmail.com>
2021-08-26 00:19:32 +0800

    LYn的工具类

--- src/com/lyn/util/MyArrayList.java
+++ src/com/lyn/util/MyArrayList.java
@@ -1,4 +1,28 @@
 package com.lyn.util;
 
-public class MyArrayList {
+public class MyArrayList<AnyType> implements Iterable<AnyType> {
+    private static final int DEFALUT_CAPACITY=10;
+    private int theSize;
+    private AnyType[] theItems;
+
+
+    public MyArrayList(){
+        doclear();
+    }
+    public void clear(){
+        doclear();
+    }
+
+    private void doclear() {
+        theSize=0;ensureCapacity(DEFALUT_CAPACITY);
+    }
+    public int size(){
+        return theSize;
+    }
+
+
+    private void ensureCapacity(int defalutCapacity) {
+    }
+
+
 }
