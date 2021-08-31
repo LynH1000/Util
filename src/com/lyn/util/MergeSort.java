2236760f481fe4fce66afdcb845c08c62c4bea43
LynH1000 <45010496+lynh1000@users.noreply.github.com>
2021-08-31 22:23:15 +0800

    归并排序

--- src/com/lyn/util/MergeSort.java
+++ src/com/lyn/util/MergeSort.java
@@ -1,27 +1,41 @@
 package com.lyn.util;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
-        int L=0,R = 0;
-        int arr[] = new int[0];
-        process( L, R,arr);
+
+        int arr[] = {1, 3, 2, 5, 4, 9};
+        int L = 0, R = arr.length-1;
+        process(arr, L, R);
+        Arrays.stream(arr).forEach( System.out::println);
     }
 
-    private static void process( int[] arr,int L, int R) {
-        if (L==R)
+    private static void process(int[] arr, int L, int R) {
+        if (L == R)
             return;
-        int mid=L+((R-L)>>1);
-        process(arr,L,mid);
-        process(arr,mid+1,R);
-        merge(arr,L,mid,R);
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
 
     }
 
     private static void merge(int[] arr, int L, int mid, int R) {
-        int help[];
-        int i=0;
-        int point1=L;
-        int point2=R;
+        int help[] = new int[R - L + 1];
+        int i = 0;
+        int point1 = L;
+        int point2 =mid+1;
+        while (point1 <= mid && point2 <= R) {
+            help[i++] = arr[point1] <= arr[point2] ? arr[point1++] : arr[point2++];
+        }
+        while (point1 <= mid)
+            help[i++] = arr[point1++];
+        while (point2 <= R)
+            help[i++] = arr[point2++];
+        for (int j = 0; j < help.length; j++) {
+            arr[L + j] = help[j];
+        }
     }
 
 
