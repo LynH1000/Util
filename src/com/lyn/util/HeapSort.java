cceebc9a134a33d1f3a50b4738154affe911504d
LynH1000 <45010496+lynh1000@users.noreply.github.com>
2021-09-11 22:46:46 +0800

    桶排序和堆排序

--- src/com/lyn/util/HeapSort.java
+++ src/com/lyn/util/HeapSort.java
@@ -3,17 +3,48 @@ package com.lyn.util;
 import static com.lyn.util.Sort.swap;
 
 public class HeapSort {
-    public static void heapify(int arr[],int root,int heapSize){
-        int left=root*2+1;
-        while (left<heapSize){
-            int largest=left+1<heapSize&&arr[left+1]>arr[left]?left+1:left;
-            largest=arr[largest]>arr[root]?largest:root;
+    public static void heapSort(int arr[]) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            //生成堆结构
+            heapInsert(arr, i);
+        }
+        int heapSize = arr.length;
+        //将根节点和最后一个节点交换
+        swap(arr, 0, --heapSize);
+
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+
+    }
+    //如果当前数大于他的根节点，两者交换
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[index - 1] >> 1) {
+            swap(arr, index, arr[index - 1] >> 1);
+            index = (index - 1) >> 1;
+        }
+    }
+
+
+    public static void heapify(int arr[], int root, int heapSize) {
+        int left = root * 2 + 1;
+
+        while (left < heapSize) {
+            //返回左右节点之间最大值的索引
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            //返回叶子和根节点的最大值的索引
+            largest = arr[largest] > arr[root] ? largest : root;
+            //如果根节点是最大值则不动，否则交换叶子和根节点，并将交换前的叶子节点索引值 作为下轮的根节点
             if (largest == root) {
                 break;
             }
-            Swap.swap(left,root,arr);
-            root=largest;
-            left=root*2+1;
+            swap(arr, left, root);
+            root = largest;
+            left = root * 2 + 1;
         }
     }
 }
---
+++ src/com/lyn/util/RadixSort.java
@@ -0,0 +1,49 @@
+package com.lyn.util;
+//桶排序
+public class RadixSort {
+    public static void radixsort(int arr[]){
+        if (arr == null||arr.length<2) {
+            return;
+        }
+        radixsort(arr,0,arr.length-1,maxbits(arr));
+    }
+
+    private static void radixsort(int[] arr, int L, int R, int digit) {
+        final int radix=10;
+        int i=0,j=0;
+        int bucket[]=new int [R-L+1];
+        for (int d = 1; i <= digit; i++) {
+            int count[]=new int [radix];
+            for (i=L;i<=R;i++){
+                j=getDigit(arr[i],d);
+            }
+            for ( i = 1; i < radix; i++) {
+                count[i]=count[i]+count[i-1];
+            }
+            for ( i = R; i >=L; i++) {
+                j=getDigit(arr[i],d);
+                bucket[count[j]-1]=arr[i];
+                count[j]--;
+            }
+        }
+
+
+    }
+
+    private static int getDigit(int i, int d) {
+
+        return (int) (i/ (Math.pow(10,d-1)%10));
+    }
+
+    private static int maxbits(int[] arr) {
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max=Math.max(max,arr[i]);
+        }
+        int res=0;
+
+
+        return res;
+    }
+
+}
