633c745930fb0566471253ed1b80135e94a1c4c1
Lanxi <lanxi0701@gmail.com>
2021-09-17 23:52:07 +0800

    判断回文字符串

--- src/com/lyn/util/AboutList.java
+++ src/com/lyn/util/AboutList.java
@@ -7,7 +7,7 @@ public class AboutList {
         public ListNode next;
 
     }
-
+    //反转链表
     public static ListNode  reverse(ListNode node){
         ListNode prev = null;
         ListNode curr = node;
@@ -20,6 +20,7 @@ public class AboutList {
         }
         return prev;
     }
+    //判断是否是回文链表
     public static boolean isPalidrome3(ListNode node){
         if (node == null) {
             return true;
@@ -28,19 +29,40 @@ public class AboutList {
         ListNode n2=node;
         while (n2.next!=null&&n2.next.next!=null){
             n1=n1.next;
-            n2=n2.next.next;
+            n2=n2.next.next;//运行完这个循环后n1在中点，n2在链表的最后一个节点
         }
+
+
         n2=n1.next;//n2连接到右半部分的第一个
-        n1.next=null;
-        ListNode n3=null;
-
+        n1.next=null;//将链表从终点处断开
+        ListNode temp=null;
+        //该循环相当于翻转链表的右半部分
+        while (n2!=null){
+            temp=n2.next;
+            n2.next=n1;
+            n1=n2;
+            n2=temp;
+        }
+        temp=n1;//
+        n2=node;//n2赋值为初始头结点
         boolean result=true;
-
-
-
-
-
-
+        //遍历判断是否回文
+        while (n1!=null&&n2!=null){
+            if(n1!=n2){
+                result=false;
+                break;
+            }
+            n1=n1.next;
+            n2=n2.next;
+        }
+        n1=temp.next;
+        temp.next=null;
+        while (n1!=null){
+            n2=n1.next;
+            n1.next=temp;
+            temp=n1;
+            n1=n2;
+        }
         return result;
     }
 
