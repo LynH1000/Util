f882551044a0c92bd8163e77361a70a8f3bbc12c
LynH1000 <45010496+lynh1000@users.noreply.github.com>
2021-09-26 22:54:30 +0800

    判断两个链表是否相交，并返回相交的第一个节点 （主函数）

--- src/com/lyn/util/AboutList.java
+++ src/com/lyn/util/AboutList.java
@@ -119,6 +119,24 @@ public class AboutList {
         return res;
     }
 
+    //判断两个链表是否相交，并返回相交的第一个节点 （主函数）
+    public static Node getIntersectNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if (loop1 == null || loop2 == null) {
+            return noLoop(head1, head2);
+        }
+        if (loop1 != null && loop2 != null) {
+            return bothLoop(head1, head2, loop1, loop2);
+        }
+        return null;
+    }
+
+
+    //判断链表是否有环，并获取第一个入环节点
     public static Node getLoopNode(Node head) {
         if (head == null || head.next == null || head.next.next == null) {
             return null;
@@ -138,6 +156,86 @@ public class AboutList {
             n2 = n2.next;
         }
         return n1;
+    }
+
+    //无环情况，返回两个链表的第一个相交节点
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;//用于计算两条链表的长度差
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        if (cur1 != cur2) {//如果尾结点不相等 表明两个链表必然不相交
+            return null;
+        }
+        cur1 = n > 0 ? head1 : head2;//cur1等于长链表；
+        cur2 = cur1 != head1 ? head1 : head2;//cur2等于短链表；
+        n = Math.abs(n);
+        while (n != 0) {//长链表先走n步，让2个链表处于相等长度
+            n--;
+            cur1 = cur1.next;
+        }
+
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    //有环情况
+    public static Node bothLoop(Node head1, Node head2, Node loop1, Node loop2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {
+            Node cur1 = head1;
+            Node cur2 = head2;
+            int n = 0;//用于计算两条链表的长度差
+            while (cur1.next != null) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2.next != null) {
+                n--;
+                cur2 = cur2.next;
+            }
+            if (cur1 != cur2) {//如果尾结点不相等 表明两个链表必然不相交
+                return null;
+            }
+            cur1 = n > 0 ? head1 : head2;//cur1等于长链表；
+            cur2 = cur1 != head1 ? head1 : head2;//cur2等于短链表；
+            n = Math.abs(n);
+            while (n != 0) {//长链表先走n步，让2个链表处于相等长度
+                n--;
+                cur1 = cur1.next;
+            }
+
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+
+
     }
 
 
