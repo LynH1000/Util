6c89b3de4ffe4e089edee7cb6fd864f2ff31ecae
Lanxi <lanxi0701@gmail.com>
2022-02-15 23:06:54 +0800

    前缀树

---
+++ src/com/lyn/util/structure/TrieNode.java
@@ -0,0 +1,16 @@
+package com.lyn.util.structure;
+
+/*
+ * 前缀树节点
+ * */
+public class TrieNode {
+    public int pass;
+    public int end;
+    public TrieNode[] nexts;
+
+    public TrieNode() {
+        pass = 0;
+        end = 0;
+        nexts = new TrieNode[26];
+    }
+}
